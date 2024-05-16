d7e5673d6f8533decfac513dfda584fa100ad1fc
anissarkeraraf <anissarkeraraf@gmail.com>
2024-05-16 12:38:38 +0600

    added the navber banner and footer

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-4xl">This is home</h2>
+            <Banner></Banner>
         </div>
     );
 };
--- src/Pages/Shared/NavBer/NavBer.jsx
+++ src/Pages/Shared/NavBer/NavBer.jsx
@@ -18,7 +18,7 @@ const NavBer = () => {
 
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar max-w-screen-xl fixed z-10 bg-opacity-20 bg-black text-[#FFFFFF] ">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
